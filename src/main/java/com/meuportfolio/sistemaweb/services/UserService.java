64ea10dc4d161a7a6805d0a84467c2ca2f37842d
Daniel Hirata <hiratadanielyuji@gmail.com>
2026-07-22 16:09:41 -0300

    Exception handling Update

--- src/main/java/com/meuportfolio/sistemaweb/services/UserService.java
+++ src/main/java/com/meuportfolio/sistemaweb/services/UserService.java
@@ -13,25 +13,27 @@ import com.meuportfolio.sistemaweb.repositories.UserRepository;
 import com.meuportfolio.sistemaweb.services.exceptions.DatabaseException;
 import com.meuportfolio.sistemaweb.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long Id) {
 		Optional<User> obj = repository.findById(Id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(Id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
@@ -40,19 +42,23 @@ public class UserService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
-		
+
 	}
-	
+
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id); 
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
 }
